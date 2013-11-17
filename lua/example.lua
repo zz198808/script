51212671b6f88fa2469799419c9f99f1660b495d
dolfly <dolfly@foxmail.com>
2013-11-17 17:55:59 +0800

    * add lua example script

---
+++ lua/example.lua
@@ -0,0 +1,18 @@
+#!/usr/bin/env lua
+--
+--------------------------------------------------------------------------------
+--         FILE:  example.lua
+--        USAGE:  ./example.lua 
+--  DESCRIPTION:  an example lua script
+--      OPTIONS:  ---
+-- REQUIREMENTS:  ---
+--         BUGS:  ---
+--        NOTES:  ---
+--       AUTHOR:   (), <>
+--      COMPANY:  
+--      VERSION:  1.0
+--      CREATED:  2013/11/17 17时55分23秒 CST
+--     REVISION:  ---
+--------------------------------------------------------------------------------
+--
+
